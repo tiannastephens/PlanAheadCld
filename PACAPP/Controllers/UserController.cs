a22763d6d855a99c83487e0cd4b8300ddb86a4f8
tianna stephens <tianna.t.stephens@outlook.com>
2021-04-11 06:36:49 -0700

    initial commit

---
+++ PACAPP/Controllers/UserController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PACAPP.Models;
+
+namespace PACAPP.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+        public IActionResult User()
+        {
+            return View();
+        }
+
+        //CREATE
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Add(User u)
+        {
+            return RedirectToAction("UserDetails");
+        }
+
+        //READ 
+        public IActionResult UserDetails(int Id)
+        {
+            User u = new User();
+            u.Id = 1;
+            u.UserName = "Juice";
+            u.Password = "JJ";
+            return View(u);
+        }
+
+        //UPDATE
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            User u = new User();
+            u.Id = 1;
+            u.UserName = "Juice";
+            u.Password = "JJ";
+            return View(u);
+        }
+        [HttpPost]
+        public IActionResult Edit(User u)
+        {
+            return RedirectToAction("UserDetails");
+        }
+    }
+}
---
+++ PACAPP/Models/User.cs
@@ -0,0 +1,16 @@
+﻿namespace PACAPP.Models
+{
+    public class User///POCO
+    {
+        //FIELDS & PROPERTIES 
+        public int Id            { get; set; }
+
+        public string UserName   { get; set; }
+
+        public string Password   { get; set; }
+
+        //COSTRUCTORS
+
+        //METHODS 
+    }
+}
